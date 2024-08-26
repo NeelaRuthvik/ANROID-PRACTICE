444ab9f9d9d1524c8472a939816996e3d210514e
1742414 -Ruthvik Chandra <118880935+NeelaRuthvik@users.noreply.github.com>
2024-08-26 07:58:03 +0530

    Add files via upload

---
+++ DAY 1/Globalvariable.kt
@@ -0,0 +1,16 @@
+package `DAY 1`
+
+val globalVariable = "I'm a global variable"
+
+fun exampleFunction1() {
+    println(globalVariable)  // Accessible here
+}
+
+fun anotherFunction2() {
+    println(globalVariable)  // Also accessible here
+}
+
+fun main (){
+    exampleFunction1()
+    anotherFunction2()
+}
