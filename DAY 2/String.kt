6abb5be8d2b04b3d139ef0a9e830902a058ecdef
1742414 -Ruthvik Chandra <118880935+NeelaRuthvik@users.noreply.github.com>
2024-08-26 07:59:00 +0530

    Add files via upload

---
+++ DAY 2/String.kt
@@ -0,0 +1,19 @@
+package `DAY 2`
+
+//fun main() {
+//    print(" Ruthvik \n Ruthvik \n Ruthvik  \n Ruthvik \n Ruthvik")
+//}
+
+//fun main() {
+//    print("Ruthvik \n \t Ruthvik \n \t \t Ruthvik \n \t \t \t Ruthvik")
+//}
+
+fun main() {
+    print("""
+               Ruthvik
+               
+        Ruthvik       Ruthvik
+        
+               Ruthivk
+    """.trimIndent())
+}
